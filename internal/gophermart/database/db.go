c3ae9abb2911ff8a69adb0a2a479c298bf24f3ee
Liudmila <l.litvinski@gmail.com>
2024-08-16 00:00:03 +0300

    add enums creation

--- internal/gophermart/database/db.go
+++ internal/gophermart/database/db.go
@@ -25,11 +25,11 @@ func CreateTable(db *sql.DB, ctx context.Context) error { // user
 		return err
 	}
 
-	// statusType := `create type status_type_enum as enum ('new', 'processing', 'invalid', 'processed');`
-	// _, err = db.ExecContext(ctx, statusType) // , model.StatusNew, model.StatusProcessing, model.StatusInvalid, model.StatusProcessed)
-	// if err != nil {
-	// 	return err
-	// }
+	statusType := `create type status_type_enum as enum ('new', 'processing', 'invalid', 'processed');`
+	_, err = db.ExecContext(ctx, statusType) // , model.StatusNew, model.StatusProcessing, model.StatusInvalid, model.StatusProcessed)
+	if err != nil {
+		return err
+	}
 
 	sqlStOrder := `create table if not exists "order"
 		(id serial primary key,
@@ -46,11 +46,11 @@ func CreateTable(db *sql.DB, ctx context.Context) error { // user
 	}
 
 	// начисления и списания
-	// transactionType := `create type transaction_type_enum as enum ('accrual', 'withdrawal');`
-	// _, err = db.ExecContext(ctx, transactionType) // , model.TransactionAccrual, model.TransactionWithdrawal)
-	// if err != nil {
-	// 	return err
-	// }
+	transactionType := `create type transaction_type_enum as enum ('accrual', 'withdrawal');`
+	_, err = db.ExecContext(ctx, transactionType) // , model.TransactionAccrual, model.TransactionWithdrawal)
+	if err != nil {
+		return err
+	}
 
 	sqlStLoyalty := `create table if not exists loyalty_system
 		(id serial primary key,
@@ -89,11 +89,11 @@ func CreateTable(db *sql.DB, ctx context.Context) error { // user
 		return err
 	}
 
-	// rewardType := `create type reward_type_enum as enum ('percent', 'points');`
-	// _, err = db.ExecContext(ctx, rewardType) //, model.RewardTypePercent, model.RewardTypePoints)
-	// if err != nil {
-	// 	return err
-	// }
+	rewardType := `create type reward_type_enum as enum ('percent', 'points');`
+	_, err = db.ExecContext(ctx, rewardType) //, model.RewardTypePercent, model.RewardTypePoints)
+	if err != nil {
+		return err
+	}
 
 	sqlStReward := `create table if not exists reward
 		(id serial primary key,
