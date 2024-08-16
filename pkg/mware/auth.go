0e4e068e7aa1c313f72ee9fdd13e4430a4fe3ece
Liudmila <l.litvinski@gmail.com>
2024-08-17 00:31:38 +0300

    fix token parsing

--- pkg/mware/auth.go
+++ pkg/mware/auth.go
@@ -20,21 +20,26 @@ func AuthMwr(h http.HandlerFunc, secret []byte) http.HandlerFunc {
 		}
 
 		// проверяем доступы
-		if authHeaderValue != "" {
-			bearerToken := strings.Split(authHeaderValue, " ")
-			//log.Println("bearerToken:", bearerToken[1])
-			if len(bearerToken) == 2 {
-				login, ok := security.VerifyToken(secret, bearerToken[1])
-				if !ok {
-					w.WriteHeader(http.StatusUnauthorized) // пользователь не аутентифицирован
-					return
-				} else {
-					r.Header.Set("x-user", login)
-					h.ServeHTTP(w, r) // передали следующей функции, которую мы обрамляем middleware'ом
-					return
-				}
-			}
+		//if authHeaderValue != "" {
+		bearerToken := strings.Split(authHeaderValue, " ")
+		log.Println("bearerToken:", bearerToken, len(bearerToken))
+		if len(bearerToken) != 2 || bearerToken[0] != "Bearer" {
+			w.WriteHeader(http.StatusUnauthorized) // пользователь не аутентифицирован
+			return
 		}
+
+		// if len(bearerToken) == 2 {
+		login, ok := security.VerifyToken(secret, bearerToken[1])
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized) // пользователь не аутентифицирован
+			return
+		} else {
+			r.Header.Set("x-user", login)
+			h.ServeHTTP(w, r) // передали следующей функции, которую мы обрамляем middleware'ом
+			return
+		}
+		// }
+		//}
 
 	}
 	// возвращаем функционально расширенный хендлер
